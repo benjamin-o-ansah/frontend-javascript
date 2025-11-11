1181c176cfe31bf3583db05a1d93593392781f82
benjamin-o-ansah <benjaminoansah@gmail.com>
2025-11-11 08:40:47 +0000

    added main.js to the project

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,35 @@
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+
+namespace Subjects {
+  // Export teacher object
+  export const cTeacher: Teacher = {
+    firstName: 'John',
+    lastName: 'Doe',
+    experienceTeachingC: 10
+  };
+
+  // Export subject instances
+  export const cpp = new Cpp();
+  export const java = new Java();
+  export const react = new React();
+
+  // C++ subject
+  console.log('C++');
+  cpp.setTeacher(cTeacher);
+  console.log(cpp.getRequirements());
+  console.log(cpp.getAvailableTeacher());
+
+  // Java subject
+  console.log('Java');
+  java.setTeacher(cTeacher);
+  console.log(java.getRequirements());
+  console.log(java.getAvailableTeacher());
+
+  // React subject
+  console.log('React');
+  react.setTeacher(cTeacher);
+  console.log(react.getRequirements());
+  console.log(react.getAvailableTeacher());
+}
